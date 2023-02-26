8ea23fe522eac4f5c08c183c526e89bd9c44a994
mrizkiaditya <muhammadrizkiaditya1@gmail.com>
2023-02-26 10:24:46 +0700

    full code

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Beranda from "./components/Beranda";
+import ManajemenBuku from "./components/ManajemenBuku";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Beranda />} />
+          <Route path="/manajemen-buku" exact element={<ManajemenBuku />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
